89f1a5eb3cefda1ef682c93da20efaedd6936328
eehd80 <eehd80yul@gmail.com>
2021-09-14 01:01:04 +0900

    d

--- pnuh-center/include/js/comm.js
+++ pnuh-center/include/js/comm.js
@@ -2,6 +2,4 @@ $(function () {
     // 공지사항
     // $(".mNews .successiveTab").remove();
     // $(".mNews .successiveTab .on").remove();
-
-    $(".slick-track").css("width", "3220px");
 });
--- pnuh-center/include/js/reN_layout.js
+++ pnuh-center/include/js/reN_layout.js
@@ -1005,15 +1005,36 @@ $(function () {
     });
 
     // tab
-    $(document).on("click", "li[id^=tab]", function () {
-        var varObj = $(this);
-        var varObjRel = varObj.attr("rel");
-        $("li[id^=tab]").each(function () {
-            $(this).removeClass("on");
-            $("#" + $(this).attr("rel")).hide();
-        });
-        varObj.addClass("on");
-        $("#" + varObjRel).show();
-        return false;
+    // $(document).on("click", "li[id^=tab]", function () {
+    //     var varObj = $(this);
+    //     var varObjRel = varObj.attr("rel");
+    //     $("li[id^=tab]").each(function () {
+    //         $(this).removeClass("on");
+    //         $("#" + $(this).attr("rel")).hide();
+    //     });
+    //     varObj.addClass("on");
+    //     $("#" + varObjRel).show();
+
+    //     // $(".tab-wrap").slick("setPosition");
+    //     return false;
+    // });
+
+    $(".successiveTab > button").click(function () {
+        var $this = $(this);
+        var index = $this.index();
+
+        $this.addClass("active");
+        $this.siblings("button.active").removeClass("active");
+
+        var $outer = $this.closest(".tab-wrap");
+        var $current = $outer.find(" > .tabs > .tab.active");
+        var $post = $outer.find(" > .tabs > .tab").eq(index);
+
+        $current.removeClass("active");
+        $post.addClass("active");
+        // 위의 코드는 탭메뉴 코드입니다.
+
+        $(".slider").slick("setPosition");
+        // 탭 페이지 안에서 slick 사용시 – slick이 첫페이지에 있지 않으면 slick의 첫번째 이미지가 보이지 않고 2번째 부터 도는것을 확인 할 수 있다. 해당 문제는 탭이 active가 된 후 그 페이지에 slick이 있다면 = slick의 위치를 수동으로 새로 고쳐줘야 한다.
     });
 });
